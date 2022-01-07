e61854e28a91d9b651e3c4ece0fd8d1fee8bb906
Micheal Pearce <michealpearce@outlook.com>
2022-01-07 18:57:29 +0000

    removed examples

--- examples/ssg/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { setupLayouts } from 'virtual:generated-layouts'
-import generatedRoutes from 'virtual:generated-pages'
-import { ViteSSG } from 'vite-ssg'
-import App from './App.vue'
-import './index.css'
-
-const routes = setupLayouts(generatedRoutes)
-
-export const createApp = ViteSSG(App, { routes })
